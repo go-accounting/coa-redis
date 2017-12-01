fe2597f39dd3bc05233b6d9b9f42c9a258cf209f
Marcos César de Oliveira <mcesarhm@gmail.com>
2017-12-01 10:09:57 -0200

    Initial commit

---
+++ cr.go
@@ -0,0 +1,47 @@
+package coaredis
+
+import (
+	"strings"
+
+	"github.com/go-accounting/coa"
+	"github.com/go-redis/redis"
+	"github.com/pkg/errors"
+)
+
+type redisStore struct {
+	c      redis.UniversalClient
+	prefix *string
+}
+
+func NewStore(master string, addrs []string, prefix *string) (coa.KeyValueStore, error) {
+	client := redis.NewUniversalClient(&redis.UniversalOptions{
+		MasterName: master,
+		Addrs:      addrs,
+	})
+	if err := client.Ping().Err(); err != nil {
+		return nil, errors.Wrap(err, "ping failed")
+	}
+	if !strings.HasSuffix(*prefix, "/") {
+		*prefix += "/"
+	}
+	return redisStore{client, prefix}, nil
+}
+
+func (rs redisStore) Get(key []byte) ([]byte, error) {
+	cmd := rs.c.Get(*rs.prefix + string(key))
+	if cmd.Err() == redis.Nil {
+		return nil, nil
+	}
+	if cmd.Err() != nil {
+		return nil, cmd.Err()
+	}
+	b, err := cmd.Bytes()
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (rs redisStore) Put(key []byte, value []byte) error {
+	return rs.c.Set(*rs.prefix+string(key), value, 0).Err()
+}
---
+++ plugin/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	coaredis "github.com/go-accounting/coa-redis"
+)
+
+func NewKeyValueStore(settings map[interface{}]interface{}, sp *string) (interface{}, error) {
+	v := settings["Addresses"].([]interface{})
+	addrs := make([]string, len(v))
+	for i, a := range v {
+		addrs[i] = a.(string)
+	}
+	master, _ := settings["Master"].(string)
+	return coaredis.NewStore(master, addrs, sp)
+}
